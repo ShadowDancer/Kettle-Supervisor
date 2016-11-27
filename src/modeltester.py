d7730bce51e4086598f7cbc4d83f914f9b8ac24a
Przemyslaw Onak <przemekonak@gmail.com>
2016-11-27 18:50:00 +0100

    5 fold crossvalidation
    closses #13

---
+++ src/modeltester.py
@@ -0,0 +1,25 @@
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
+def test_model(model, X, Y, folds = 5):
+    cv = StratifiedKFold(n_splits=5)
+    return cross_val_score(model, X, Y, cv=cv, n_jobs=-1);
+    
+def print_score(scores):
+    return "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+
+if __name__ == '__main__':
+    
+    from sklearn import svm
+    from datetime import datetime
+    from loader import Loader
+    
+    print str(datetime.now()) + " loading data"
+    l = Loader();
+    X, Y = l.load_data();   
+
+    print str(datetime.now()) + " testing"
+    scores = test_model(svm.SVC(C=1), X, Y)
+    print str(datetime.now()) + " done"
+    print print_score(scores)
+
